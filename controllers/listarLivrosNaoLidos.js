3ec2f46a3ed5fbf91fa40c8078b986ac47054ff4
Marina Porto <marinaporto334@gmail.com>
2022-04-25 15:23:41 -0300

    Adicionando o README

--- controllers/listarLivrosNaoLidos.js
+++ controllers/listarLivrosNaoLidos.js
@@ -2,8 +2,8 @@ const livros = require('../database')
 const read = require('readline-sync')
 
 const listarLivrosNaoLidos = () => {
-    const primeiraResposta = read.question('Deseja ver a lista de desejos? S/N').toUpperCase()
-    if (primeiraResposta === 'S') {
+    const pResposta = read.question('Deseja ver a lista de desejos? S/N').toUpperCase()
+    if (pResposta === 'S') {
         const livrosQueroLer = livros.filter(livro => livro.leu === false)
         console.table(livrosQueroLer)
 
--- controllers/listarRecomendados.js
+++ controllers/listarRecomendados.js
@@ -2,8 +2,8 @@ const livros = require('../database')
 const read = require('readline-sync')
 
 const listarRecomendados = () => {
-    const primeiraResposta = read.question('Deseja ver os livros recomendados? S/N').toUpperCase()
-    if (primeiraResposta === 'S') {
+    const priResposta = read.question('Deseja ver os livros recomendados? S/N').toUpperCase()
+    if (priResposta === 'S') {
         const livrosRecomendados = livros.filter(livro => livro.recomenda === true)
         console.table(livrosRecomendados)
 
